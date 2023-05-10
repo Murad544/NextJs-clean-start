d41c62d24cfd171b3b49b1dd061f9cd92283f5b9
Murad544 <murad.bayramov259@gmail.com>
2023-05-10 11:20:15 +0400

    first commit

---
+++ useful/index.ts
@@ -0,0 +1,15 @@
+// Akt
+
+// let goldBrokerage = await axios({
+//     method: 'get',
+//     baseURL: `http://172.20.0.105:8083/api/Brokerages/GetBrokerages`,
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${TOKEN}`,
+//         'api-key': '790ED398-3C37-4AB8-ADB7-78A4657DF703',
+//     }
+// })
+
+// const handleChange = (e) => {
+//     setFormValues({ ...formValues, [e.target.id]: e.target.value });
+// };
